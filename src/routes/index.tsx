69f21cf2439fcb8b38655f016f0b18e8ac13bee7
Eduardo <du_du.mba@hotmail.com>
2020-09-07 21:37:35 +0100

    commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Router: React.FC = () => (
+  <Switch>
+    <Route path="" />
+  </Switch>
+);
+
+export default Router;
+
