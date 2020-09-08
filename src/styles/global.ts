f43efcaa226e869100c8516b803969e25c9d68da
Eduardo <du_du.mba@hotmail.com>
2020-09-08 21:18:03 +0100

    create Global-style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import githubBackground from '../assets/bg.svg';
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -9,7 +11,21 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #F0F0F5;
+    background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
