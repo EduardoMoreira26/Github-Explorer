69c8c466270be11b69336f79a21f7d4219fe3014
Eduardo <du_du.mba@hotmail.com>
2020-09-09 22:04:32 +0100

    update project

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
 
@@ -16,6 +18,47 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+            alt="Diego Fernandes"
+          />
+          <div>
+            <strong>rocketseat/unform/</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms! 🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+            alt="Diego Fernandes"
+          />
+          <div>
+            <strong>rocketseat/unform/</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms! 🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+            alt="Diego Fernandes"
+          />
+          <div>
+            <strong>rocketseat/unform/</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms! 🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { shade } from 'polished';
+
 export const Title = styled.h1`
   font-size: 48px;
   color: #3A3A3A;
@@ -21,11 +23,81 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
   }
 
   button {
     width: 210px;
     height: 70px;
+    background: #04D361;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04D361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px 0;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px)
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+      margin: 0 24px;
+
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
   }
 `;
 
