4e48f89566b4410862823db208b4fd4be5942acb
Eduardo <du_du.mba@hotmail.com>
2020-09-09 21:08:07 +0100

    udpate dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>
+
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  );
 }
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,4 +3,29 @@ import styled from 'styled-components';
 export const Title = styled.h1`
   font-size: 48px;
   color: #3A3A3A;
+  max-width: 450px;
+  line-height: 56px;
+
+  margin-top: 80px;
 `;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+  }
+`;
+
