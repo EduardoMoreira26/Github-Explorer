12a9b0c166d0da5d2ef60514ea896441bd84fd43
Eduardo <du_du.mba@hotmail.com>
2020-09-15 21:23:11 +0100

    crate repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useRouteMatch, Link } from "react-router-dom";
-import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Header } from './styles';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -14,13 +14,50 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <Header>
-      <img src={logoImg} alt="Github Explorer" />
-      <Link to="/dashboard">
-        <FiChevronLeft size={16} />
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
         Voltar
       </Link>
-    </Header>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars3.githubusercontent.com/u/62812791?s=400&u=4d7d19e7b006a4979fac2aaf4762218517621cc2&v=4" alt="Eduardo" />
+          <div>
+            <strong>Eduardo Moreira</strong>
+            <p>Descrição do repositorio</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>dasd</strong>
+            <p>sadasd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 }
 
