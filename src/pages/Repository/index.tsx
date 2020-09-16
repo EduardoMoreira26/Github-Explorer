4f286e85cf63219c18cea2199dc5b1478c9a7429
Eduardo <du_du.mba@hotmail.com>
2020-09-16 21:26:59 +0100

    project

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from "react-router-dom";
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,8 +13,42 @@ interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  user: {
+    login: string;
+  }
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      console.log(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      console.log(response.data);
+    });
+
+  }, [params.repository]);
 
   return (
     <>
